223827f888406d3b556551b03ea12dc38833c2ca
andreshaitit <andreshaitit99@gmail.com>
2025-03-26 14:36:14 -0300

    fix .env

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -11,8 +11,6 @@ module.exports = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        synchronize: process.env.DB_SYNCHRONIZE === "true",
-        logging: process.env.DB_LOGGING === "true",
         entities: [Users, Cryptos],
     }),
 };
